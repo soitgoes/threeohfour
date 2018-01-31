fa6c43794cfc23c3d62606e80f70f12837a9da26
Martin Murphy <martin.murphy@whiteboard-it.com>
2018-01-31 11:55:25 -0600

    Update dependancies

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 #!/usr/bin/env node
 
-var bogart  = require('bogart-edge')
 var program = require('commander')
 var args = process.argv
-var router = bogart.router()
 
 program
-  .option('-u, --url', 'Url to redirect to')
+  .option('-u, --url <url>', 'Url to redirect to')
   .option('-p, --port <n>', 'Port to listen on', parseInt)
   .parse(process.argv);
 
 console.log("Listening on port: " + program.port)
+console.log("Redirecting to:" + program.url)
 
 if (!program.url){
   console.log("Must supply a url")
   return;
 }
+const express = require('express')
+const app = express()
 
-router.get(/.*/, function(req) {
-  return bogart.redirect(program.url)
-});
+app.get('/', (req, res) => res.redirect(program.url))
 
-var app = bogart.app();
-app.use(router); // Our router
 var port = program.port ? program.port : 8989;
-app.start({port: port});
+app.listen(port, () => console.log(`ThreeOhFour Redirector listening on port ${port}!`))
