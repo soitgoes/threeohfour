dfd5c2846b4cb23f65134d1a4665f0f1095ac9e4
Martin Murphy <martin.murphy@whiteboard-it.com>
2018-01-31 10:28:50 -0600

    initial add

---
+++ app.js
@@ -0,0 +1,25 @@
+var bogart  = require('bogart')
+var program = require('commander')
+var args = process.argv
+var router = bogart.router()
+
+program
+  .option('-u, --url', 'Url to redirect to')
+  .option('-p, --port <n>', 'Port to listen on', parseInt)
+  .parse(process.argv);
+
+console.log("Listening on port: " + program.port)
+
+if (!program.url){
+  console.log("Must supply a url")
+  return;
+}
+
+router.get(/.*/, function(req) {
+  return bogart.redirect(program.url)
+});
+
+var app = bogart.app();
+app.use(router); // Our router
+var port = program.port ? program.port : 8989;
+app.start({port: port});
